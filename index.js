752595f853d67d5fb614abebaa65ba97c1470d71
MADMONKEY\석규 <tlqkfkak88@naver.com>
2019-02-10 15:05:13 +0900

    make my first server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`running on : localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send('Hello~');
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+}
+
+app.listen(PORT, handleListening);
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
