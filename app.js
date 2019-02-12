be40da1860de13af5ed1e39ffe2f55f45da8721e
MADMONKEY\석규 <tlqkfkak88@naver.com>
2019-02-12 20:21:10 +0900

    divide  routers

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello~ world~');
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenMW = (req, res, next) => {
-    console.log("between");
-    next();
-}
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -23,10 +17,8 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(betweenMW);
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
